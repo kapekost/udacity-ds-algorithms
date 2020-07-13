4a10dccc8e45f147b5bc994cca7a741ef3ce3359
kapekost <kapekost@gmail.com>
2020-07-13 23:36:52 +0100

    project 2 analysis and tests

--- project2/problem_2.py
+++ project2/problem_2.py
@@ -22,7 +22,7 @@ class fileExplorer():
         Returns:
         a list of paths
         """
-        if (suffix is None or path is None):
+        if (suffix is None or path is None or path == '' or suffix == ''):
             return None
 
         file_list = None
@@ -30,36 +30,64 @@ class fileExplorer():
         return file_list
 
     def search_in_dir(self, suffix, path, file_list):
+        """
+        Search the files for the given suffix within the folder (path)
+        add the fils in the input file_list
+
+        for each folder search again with the same function
+
+        Args:
+        suffix(str): suffix if the file name to be found
+        path(str): path of the file system
+        file_list(array): the array to append files that are found
+
+        Returns:
+        a list of paths
+        """
         try:
             directory_list = os.listdir(path)
         except FileNotFoundError as error:
             print(error)
             return
 
-        # print(directory_list)
         sub_dirs = []
         for item in directory_list:
-            # item = path+"/"+item
-            # print("processing> {}".format(item))
             if os.path.isfile(path+"/"+item):
-                # print("found file {}".format(path+"/"+item))
                 if item.endswith(suffix):
-                    # print("added")
                     file_list.append(path+"/"+item)
             elif os.path.isdir(path+"/"+item):
-                # print("found folder {}".format(path+"/"+item))
-                sub_dirs.append(path+"/"+item)
-        # print("files up to here: {}".format(file_list))
-        # print("folders up to here: {}".format(sub_dirs))
-        for sub_dir in sub_dirs:
-            # print('going in subdir: {}'.format(sub_dir))
-            sub_files = self.search_in_dir(suffix, sub_dir, file_list)
-            # print("after recurse:{}".format(sub_files))
-            # for sub_file in sub_files:
-            #     file_list.append(sub_file)
+                self.search_in_dir(suffix, path+"/"+item, file_list)
 
         return file_list
 
 
+print("testing for testdir and files .c")
 file_explorer = fileExplorer()
-print("c files: {}".format(file_explorer.find_files(".c", "testdir")))
+
+assert file_explorer.find_files('.c', 'testdir') == \
+    ['testdir/subdir1/a.c', 'testdir/subdir3/subsubdir1/b.c', 'testdir/subdir5/a.c',
+        'testdir/t1.c'],  f" files: .c"
+
+print("testing for testdir and files .h")
+assert file_explorer.find_files('.h', 'testdir') == \
+    ['testdir/subdir1/a.h', 'testdir/subdir3/subsubdir1/b.h', 'testdir/subdir5/a.h',
+        'testdir/t1.h'],  f" files: .h"
+
+print("testing for testdir and files .a to be none")
+assert file_explorer.find_files('.', 'testdir') == \
+    [],  f" files: .h"
+
+print("testing for testdir and empty suffix")
+assert file_explorer.find_files('', 'testdir') == \
+    None,  f" files: ''"
+
+print("testing for testdir and no suffix")
+assert file_explorer.find_files(None, 'testdir') == \
+    None,  f" files: ''"
+print("testing for empty folder and .c suffix")
+assert file_explorer.find_files('.c', '') == \
+    None,  f" files: ''"
+
+print("testing for None folder and .c suffix")
+assert file_explorer.find_files('.c', None) == \
+    None,  f" files: ''"
