60ed7e848d8e35969c61ff824fe1f79650251f61
kapekost <kapekost@gmail.com>
2020-07-14 15:40:24 +0100

    problem 6 analysis

--- project2/explanation_6.md
+++ project2/explanation_6.md
@@ -1,2 +1,37 @@
+## Problem 6
 
+Space complexity and logic:
 
+basic linked list:
+The Node hold a value and the next node
+The linked list length determines the space
+
+appending and size calculations use 1-2 vars for node and counter
+
+- Union:
+  Thought to use the Set as it will have performance of O(1) and we won't have to perform a lookup for existing values
+  Then structuring a new linked list to return seems faster than re-arranging nodes that are connected between each other, parsing one node will need our algorithm to pass through all the ones between (and move pointers)
+
+it will have the size of the unique elements picked from both arrays
+worst case will be all of them being unique. sizeA + SizeB
+
+- Intersection:
+  for the intersection we use 2 sets, one for each input
+  Using set here allows us to do the intersection with the & operator and then structure a new linked list to return
+  and one more for the result of the intersection,
+  we can optimize space but would make things less readable
+
+Time:
+
+- Union:
+  looping through list one to add in our set (O(n)+1) (n elements of 1st list)
+  looping through list2 and add in set (O(m)+1) (m elemenets of 2nd list)
+  loop through the generated set to create and append nodes (O(k)+(O(l))) k the elements in the set, and l the size of the list where the new Node will attach at the end
+  Complexity => O(n)
+
+- intersection:
+  looping through list one to add in set1 (O(n)+1) (n elements of 1st list)
+  looping through list2 and add in set2 (O(m)+1) (m elemenets of 2nd list)
+  perform Python & (O(n)) # https://wiki.python.org/moin/TimeComplexity%20%28SetCode%29
+  loop through the generated set to create and append nodes (O(k)+(O(l))) k the elements in the set, and l the size of the list where the new Node will attach at the end
+  Complexity => O(n)
--- project2/problem_6.py
+++ project2/problem_6.py
@@ -43,22 +43,32 @@ class LinkedList:
 
 def union(llist_1, llist_2):
     """
-    Combine the 2 input LinkedLists to provide 
+    Combine the 2 input LinkedLists to provide
     a new with only unique elements found in both
 
     Args:
         llist_1(class:LinkedList): the first LinkedList
         llist_2(class:LinkedList): the second LinkedList
     """
+    if llist_1.head == None:
+        return llist_2
+    if llist_2.head == None:
+        return llist_1
+
     new_items = set()
+
     current = llist_1.head
+    new_items.add(current.value)
     while current.next is not None:
-        new_items.add(current.value)
+        new_items.add(current.next.value)
         current = current.next
+
     current = llist_2.head
+    new_items.add(current.value)
     while current.next is not None:
-        new_items.add(current.value)
+        new_items.add(current.next.value)
         current = current.next
+
     final_linked_list = LinkedList()
     for i in new_items:
         final_linked_list.append(i)
@@ -66,15 +76,22 @@ def union(llist_1, llist_2):
 
 
 def intersection(llist_1, llist_2):
+    if llist_1.head == None:
+        return LinkedList()
+    if llist_2.head == None:
+        return LinkedList()
+
     new_items1 = set()
     new_items2 = set()
     current = llist_1.head
+    new_items1.add(current.value)
     while current.next is not None:
-        new_items1.add(current.value)
+        new_items1.add(current.next.value)
         current = current.next
     current = llist_2.head
+    new_items2.add(current.value)
     while current.next is not None:
-        new_items2.add(current.value)
+        new_items2.add(current.next.value)
         current = current.next
     # use python's & to get the intersection of the 2 sets
     new_items = new_items1 & new_items2
@@ -99,11 +116,13 @@ if __name__ == "__main__":
     for i in element_2:
         linked_list_2.append(i)
 
-    print(union(linked_list_1, linked_list_2))
-    print(intersection(linked_list_1, linked_list_2))
+    print("union:", union(linked_list_1, linked_list_2))
+    print("intersection:", intersection(linked_list_1, linked_list_2))
+    # union: 32 -> 65 -> 2 -> 35 -> 3 -> 4 -> 6 -> 1 -> 9 -> 11 -> 21 ->
+    # intersection 4 -> 21 -> 6 ->
 
     # Test case 2
-    print('test 2')
+    print('test 2 have none in common, include some duplicated values')
 
     linked_list_3 = LinkedList()
     linked_list_4 = LinkedList()
@@ -117,11 +136,13 @@ if __name__ == "__main__":
     for i in element_2:
         linked_list_4.append(i)
 
-    print(union(linked_list_3, linked_list_4))
-    print(intersection(linked_list_3, linked_list_4))
+    print("union:", union(linked_list_3, linked_list_4))
+    print("intersection:", intersection(linked_list_3, linked_list_4))
+    # union: 65 -> 2 -> 35 -> 3 -> 4 -> 6 -> 1 -> 7 -> 8 -> 9 -> 11 -> 21 -> 23 ->
+    # # intersection []
 
     # Test case 3
-    print('test 3')
+    print('test 3 obvious common 1 2 3 and 4 to appear in union')
     linked_list_3 = LinkedList()
     linked_list_4 = LinkedList()
 
@@ -134,5 +155,141 @@ if __name__ == "__main__":
     for i in element_2:
         linked_list_4.append(i)
 
-    print(union(linked_list_3, linked_list_4))
-    print(intersection(linked_list_3, linked_list_4))
+    print("union:", union(linked_list_3, linked_list_4))
+    print("intersection:", intersection(linked_list_3, linked_list_4))
+    # union: 1 -> 2 -> 3 -> 4 ->
+    # intersection 1 -> 2 -> 3 ->
+
+    # Test case 4
+    print('test 4 one array empty')
+    linked_list_3 = LinkedList()
+    linked_list_4 = LinkedList()
+
+    element_1 = [1, 2, 3, 4]
+    element_2 = []
+
+    for i in element_1:
+        linked_list_3.append(i)
+
+    for i in element_2:
+        linked_list_4.append(i)
+
+    print("union:", union(linked_list_3, linked_list_4))
+    print("intersection:", intersection(linked_list_3, linked_list_4))
+    # union: 1 -> 2 -> 3 -> 4 ->
+    # intersection:
+
+    # Test case 5
+    print('test 5 the other array empty')
+    linked_list_3 = LinkedList()
+    linked_list_4 = LinkedList()
+
+    element_1 = []
+    element_2 = [1, 2, 3]
+
+    for i in element_1:
+        linked_list_3.append(i)
+
+    for i in element_2:
+        linked_list_4.append(i)
+    print("union:", union(linked_list_3, linked_list_4))
+    print("intersection:", intersection(linked_list_3, linked_list_4))
+    # union: 1 -> 2 -> 3 ->
+    # intersection:
+
+    # Test case 6
+    print('test 6 both empty')
+    linked_list_3 = LinkedList()
+    linked_list_4 = LinkedList()
+
+    element_1 = []
+    element_2 = []
+
+    for i in element_1:
+        linked_list_3.append(i)
+
+    for i in element_2:
+        linked_list_4.append(i)
+
+    print("union:", union(linked_list_3, linked_list_4))
+    print("intersection:", intersection(linked_list_3, linked_list_4))
+    # union:
+    # intersection:
+
+    # Test case 7
+    print('test 7 all common')
+    linked_list_3 = LinkedList()
+    linked_list_4 = LinkedList()
+
+    element_1 = [1, 2, 3]
+    element_2 = [1, 2, 3]
+
+    for i in element_1:
+        linked_list_3.append(i)
+
+    for i in element_2:
+        linked_list_4.append(i)
+
+    print("union:", union(linked_list_3, linked_list_4))
+    print("intersection:", intersection(linked_list_3, linked_list_4))
+    # union: 1 -> 2 -> 3 ->
+    # intersection: 1 -> 2 -> 3 ->
+
+    # Test case 8
+    print('test 8 big arrays')
+    linked_list_3 = LinkedList()
+    linked_list_4 = LinkedList()
+
+    element_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 4, 6, 34, 32, 6, 7, 78, 34,
+                 33, 42, 56, 76, 83, 94, 88, 76543, 23456, 78, 79, 38, 765, 432, 3, 45]
+    element_2 = [1, 2, 3, 6, 7, 78, 34, 32, 5, 5123,
+                 32452456, 345345, 7857, 5, 345, 534, 6756746, 653]
+
+    for i in element_1:
+        linked_list_3.append(i)
+
+    for i in element_2:
+        linked_list_4.append(i)
+
+    print("union:", union(linked_list_3, linked_list_4))
+    print("intersection:", intersection(linked_list_3, linked_list_4))
+    # union: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9 -> 5123 -> 6756746 -> 12 -> 653 -> 534 -> 32 -> 33 -> 34 -> 23456 -> 38 -> 42 -> 45 -> 345345 -> 432 -> 7857 -> 56 -> 76 -> 78 -> 79 -> 83 -> 88 -> 345 -> 94 -> 32452456 -> 765 -> 76543 ->
+    # intersection: 32 -> 1 -> 2 -> 3 -> 34 -> 5 -> 6 -> 7 -> 78 ->
+
+    # Test case 8
+    print('test 8 array with one number')
+    linked_list_3 = LinkedList()
+    linked_list_4 = LinkedList()
+
+    element_1 = [21]
+    element_2 = [1]
+
+    for i in element_1:
+        linked_list_3.append(i)
+
+    for i in element_2:
+        linked_list_4.append(i)
+
+    print("union:", union(linked_list_3, linked_list_4))
+    print("intersection:", intersection(linked_list_3, linked_list_4))
+    # union: 1 -> 21 ->
+    # intersection:
+
+    # Test case 9
+    print('test 9 array with missing number')
+    linked_list_3 = LinkedList()
+    linked_list_4 = LinkedList()
+
+    element_1 = [21, ]
+    element_2 = [1]
+
+    for i in element_1:
+        linked_list_3.append(i)
+
+    for i in element_2:
+        linked_list_4.append(i)
+
+    print("union:", union(linked_list_3, linked_list_4))
+    print("intersection:", intersection(linked_list_3, linked_list_4))
+    # union: 1 -> 21 ->
+    # intersection:
