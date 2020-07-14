de82e28d20a9bc5559af793be6bd8774e3a2f58b
kapekost <kapekost@gmail.com>
2020-07-14 19:41:46 +0100

    add problem 5 tests and explanation + append Block in the blockchain

--- project2/explanation_5.md
+++ project2/explanation_5.md
@@ -1,1 +1,41 @@
 ## Problem 5
+
+Here We need to create a list that the head will be at the last Node
+each node has a link to the previous.
+
+This link is a reference using a hash code that we get when we create a new Block(Node)
+
+Under the main class of Block_chain I decided to create a function to call the Block class that creates a Block with the given data (timestamp, data, and the previous_hash)
+
+I initialize the previous_hash with None so it will be used for the last (firstly created) Block
+
+The function takes as input an array of the data we want to create the chain with
+
+-- created a function to add nodes, which is getting called from the create_chain.
+
+a delete Block would be a loop holding the previous node while we get the one we want to delete.
+we change the pointer from the previous in the loop to point to where the one we are deleting is pointing. not sure if it's a requirement
+
+because the indexes are custom (Hashes, we need to return the hash pointing to the head of the chain, and the array (chain itself)) so we can combine the hashes to go through the cain table
+
+The space complexity is increasing based on the Blocks we are adding, containing data + timestamp + 2 hash values
+
+- append block we are not using additional resources, just the block we are creating to add
+- create chain, is the main one that creates the array (chain and triggers the add block)
+
+Time complexity:
+
+- append_block:
+  we are using the creation of Block which needs the time to
+  - create timestamp (I guess O(1) reading the value from some hash)
+  - create hash (might be O(n) with n affected by our input data)
+    we insert a node in a dictionary (O(1))
+
+complexity => O(n)
+
+- create_chain
+  with n as the number of data in our input array
+  we call the append block n times
+  O (n^2)
+
+  complexity => O(n^2)
--- project2/problem_5.py
+++ project2/problem_5.py
@@ -24,30 +24,92 @@ class Block_chain:
     def __init__(self):
         pass
 
+    def append_block(self, data, tail_node, chain={}):
+        """
+        Create a new block with data and append in the given chain
+        Args:
+            data(string): the data to add in the Block
+            tail_node(Block): the current tail of the queue(head of chain)
+            chain(dic):optional, the existing chain to append the Block
+
+        Returns:
+            the added Block, 
+            the new chain
+        """
+        # if there is nothing to append we return the expected objects
+        if data == "" or data == None:
+            return tail_node, chain
+
+        if tail_node != None:
+            previous_hash = tail_node.hash
+        else:
+            previous_hash = None
+        block_to_add = Block(time.gmtime(), data, previous_hash)
+        chain[block_to_add.hash] = block_to_add
+        return block_to_add, chain
+
     def create_chain(self, data):
         """
         Create a chain of the array in input data
-        return the tail node and chain 
+
+        Args:
+            data(string[]): the data to add in the blocks
+        Returns: 
+            the starting Blok
+            the chain 
         """
+        if data == None:
+            return
         chain = {}
-        previous_hash = None
+        tail_node = None
         for datum in data:
-            ts = time.gmtime()
-            new_block = Block(ts, datum, previous_hash)
-            current_hash = new_block.hash
-            new_block.previous_hash = previous_hash
-            chain[current_hash] = new_block
-            previous_hash = current_hash
-        return new_block, chain
+            tail_node, chain = self.append_block(datum, tail_node, chain)
+        return tail_node, chain
 
 
-block_chain = Block_chain()
-tail_block, my_chain = block_chain.create_chain(["a", "b", "c"])
-current = tail_block
-data = ""
-while current.previous_hash is not None:
-    data += current.data
-    current = my_chain[current.previous_hash]
-data += current.data
+if __name__ == "__main__":
+    # Tests
 
-print(data)
+    def get_string_test_block(current):
+        data = ""
+        while current.previous_hash is not None:
+            data += current.data + " ->"
+            current = my_chain[current.previous_hash]
+        data += current.data
+        print(data)
+        return data
+
+    block_chain = Block_chain()  # ideally we want to create a new one for each test
+    print("call create chain with a b c as data")
+    head, my_chain = block_chain.create_chain(["a", "b", "c"])
+    print("call append_block to add one more block with data = d")
+    head, my_chain = block_chain.append_block("d", head, my_chain)
+
+    data = get_string_test_block(head)
+    assert data == "d ->c ->b ->a", f"the chain should print all the nodes in reverse order"
+
+    print("call append with empty data")
+    head, my_chain = block_chain.append_block("", head, my_chain)
+    data = get_string_test_block(head)
+    assert data == "d ->c ->b ->a", f"the chain should print all the nodes in reverse order"
+
+    print("call append with none chain with 1 item")
+    head, my_chain = block_chain.append_block(None, head, my_chain)
+    data = get_string_test_block(head)
+    assert data == "d ->c ->b ->a", f"the chain should print all the nodes in reverse order"
+
+    print("call append 1000 times")
+    for i in range(1000):
+        head, my_chain = block_chain.append_block(str(i), head, my_chain)
+    data = get_string_test_block(head)
+    assert data != None, f"the chain should print all the nodes in reverse order"
+
+    block_chain2 = Block_chain()
+    print("call create chain with 1 item")
+    head, my_chain = block_chain2.create_chain(["c"])
+    data = get_string_test_block(head)
+    assert data == "c", f"the chain should print one item"
+
+    print("call create a chain with no input")
+    head, my_chain = block_chain2.create_chain("")
+    assert head == None, f"the chain should not exist"
